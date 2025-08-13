404292511c24178b8a1476e747e8c6a378726012
Ahmed Faiza <jesminnakter01@gmail.com>
2025-08-13 09:11:04 +0600

    Initial commit

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">JESMIN</div>
+          <div className="right">
+            <p>EAST NASIRABAD, CHITTAGONG</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODEWITHFAIZA</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithFaiza.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
